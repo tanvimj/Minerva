12eae798f11cca4a03cfd269e57a234860f5e73f
tanvimj <tanvimj04@gmail.com>
2026-05-12 15:44:53 +0530

    c

--- server/src/server.js
+++ server/src/server.js
@@ -15,9 +15,6 @@ app.use(express.json());
 
 app.use(cors({
   origin: [
-    'http://localhost:5500',
-    'http://127.0.0.1:5500',
-    'http://localhost:3000',
     'https://minerva-spwa.onrender.com'
   ],
   credentials: true
